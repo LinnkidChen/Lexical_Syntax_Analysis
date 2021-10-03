c5e5933e77e50bb2a6ab62575c812f7682abe346
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2021-10-03 15:03:14 +0800

    init

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,7 @@ Lexical Syntax Analysis
 Author: Linnkid_Chen
 contact: linnkid.chen@gmail.com
 
-linnkid_chen.github.∏
+Publish @ https://github.com/LinnkidChen/Lexical_Syntax_Analysis/tree/master
 
 */
 
